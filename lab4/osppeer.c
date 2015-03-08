876f0b492cdbd854ed18759feb3abe6bb38b6d92
Wei-Wei Cheng <weiwei@ubuntu.(none)>
2015-03-08 15:30:29 -0700

    Increased task buffer size

--- lab4/osppeer.c
+++ lab4/osppeer.c
@@ -37,7 +37,7 @@ static int listen_port;
  * a bounded buffer that simplifies reading from and writing to peers.
  */
 
-#define TASKBUFSIZ	4096	// Size of task_t::buf
+#define TASKBUFSIZ	65336	// Size of task_t::buf
 #define FILENAMESIZ	256	// Size of task_t::filename
 
 typedef enum tasktype {		// Which type of connection is this?
